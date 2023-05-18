f4eb702352747358bd5ed9f198f93b806de35595
Miguel <14-11127@usb.ve>
2023-05-18 16:40:02 -0400

    A ver si se ve reflejado.

---
+++ src/com/example/maspruebas/MasPruebas.java
@@ -0,0 +1,11 @@
+package com.example.maspruebas;
+
+public class MasPruebas {
+    public static void main(String[] args) {
+        System.out.println("Aquí voy a ver qué puedo programar.");
+
+        if (false)
+            System.out.println("Nada más solo quería ver.");
+        else System.out.println("Sigue la webonada.");
+    }
+}
